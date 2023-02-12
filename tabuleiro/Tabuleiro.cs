d51f0fa8e113b5beac9f92c147533b0c1fb83805
Kissyla Motta Pereira <kissylamottapereira@gmail.com>
2023-02-12 15:02:43 -0300

    cria exceção personalizada para Tabuleiro

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -14,14 +14,35 @@ namespace tabuleiro
       this.colunas = colunas;
       pecas = new Peca[linhas, colunas];
     }
-    public Peca peca(int linha, int coluna)
+    public Peca peca(Posicao pos)
     {
-      return pecas[linha, coluna];
+      return pecas[pos.Linha, pos.Coluna];
+    }
+
+    public bool existePeca(Posicao pos)
+    {
+      validarPosicao(pos);
+      return peca(pos) != null;
     }
     public void colocarPeca(Peca p, Posicao pos)
     {
       pecas[pos.Linha, pos.Coluna] = p;
       p.posicao = pos;
+    }
+    public bool posicaoValida(Posicao pos)
+    {
+      if (pos.Linha < 0 || pos.Coluna < 0 || pos.Linha >= linhas || pos.Coluna >= colunas)
+      {
+        return false;
+      }
+      return true;
+    }
+    public void validarPosicao(Posicao pos)
+    {
+      if (!posicaoValida(pos))
+      {
+        throw new TabuleiroException("Posição inválida!");
+      }
     }
   }
 }
---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+namespace tabuleiro
+{
+  class TabuleiroException : Exception
+  {
+    public TabuleiroException(string message) : base(message)
+    {
+
+    }
+  }
+}
